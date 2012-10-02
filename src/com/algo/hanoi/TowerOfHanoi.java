8a7602a1d7ec4de4820efe324d106c27e482ccc5
stratwine <tovishwanath@gmail.com>
2012-10-02 23:04:41 +0530

    Tower of Hanoi added

--- src/com/algo/hanoi/TowerOfHanoi.java
+++ src/com/algo/hanoi/TowerOfHanoi.java
@@ -9,5 +9,28 @@ package com.algo.hanoi;
  */
 public class TowerOfHanoi {
 
+    public static void hanoi(int n, String source, String temp, String dest)
+    {
+       if(n==1)
+       {
+           System.out.println("Moving disc from "+source+" to "+dest);
+       }
+       else
+       {
+         // move n-1 discs to temp to get accces to n
+           hanoi(n-1,source,dest,temp);
+           //got access to n. move it.
+           System.out.println("Moving disc from "+source+" to "+dest);
+           //now source is empty. could by used as the temp.
+         //move n-1 discs from temp
+           hanoi(n-1,temp,source,dest);
+       }
 
+    }
+
+
+  public static void main(String[] args)
+  {
+         TowerOfHanoi.hanoi(5,"Src","Aux","Dest");
+  }
 }
